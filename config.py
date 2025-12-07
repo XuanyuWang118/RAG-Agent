0217b22d7594621ca02cd3db7dfc28f76fd048bf
Xuanyu Wang <2107308484@qq.com>
2025-12-07 13:33:32 +0800

    init-commit

---
+++ config.py
@@ -0,0 +1,20 @@
+#API配置
+OPENAI_API_KEY = "sk-0161708a236043e6b18f58fe9021b351"
+OPENAI_API_BASE = "https://dashscope.aliyuncs.com/compatible-mode/v1"
+MODEL_NAME = "qwen3-max"
+OPENAI_EMBEDDING_MODEL = 
+
+# 数据目录配置
+DATA_DIR = "./data"
+
+#向量数据库配置
+VECTOR_DB_PATH = "./vector_db"
+COLLECTION_NAME = 
+
+# 文本处理配置
+CHUNK_SIZE = 
+CHUNK_OVERLAP = 
+MAX_TOKENS = 
+
+# RAG配置
+TOP_K = 
