009464efbea5d7f3ff8861c0c0cc91c19db98396
BoruiZhang <fisandpiper@outlook.com>
2025-12-11 21:20:55 +0800

    feat: 函数调用以及联网搜索

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 #API配置
-OPENAI_API_KEY = "sk-0161708a236043e6b18f58fe9021b351"
+OPENAI_API_KEY = "sk-e6dcc423c4cd497c92fe6b26047b119d"
 OPENAI_API_BASE = "https://dashscope.aliyuncs.com/compatible-mode/v1"
 MODEL_NAME = "qwen3-max"
 OPENAI_EMBEDDING_MODEL = "text-embedding-v4"
+
+# Tavily搜索API配置
+TAVILY_API_KEY = "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"
 
 # 数据目录配置
 DATA_DIR = "./data"
